6f25bbb0cf0720dc51130925317018474fef4a24
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-12 15:01:07 -0400

    error setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
     #this will nest the api under api.examplewebsite.com/
     #worry about versioning later
