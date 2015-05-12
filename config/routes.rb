24ed0b22d9e246a9347b32b0e0a3cb1def7be66a
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-12 15:10:05 -0400

    added devise user and successfully migrated db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
     #this will nest the api under api.examplewebsite.com/
     #worry about versioning later
@@ -13,3 +14,26 @@ Rails.application.routes.draw do
   end
 
 end
+
+=begin 2. Ensure you have defined root_url to *something* in your config/routes.rb.
+     For example:
+
+       root to: "home#index"
+
+  3. Ensure you have flash messages in app/views/layouts/application.html.erb.
+     For example:
+
+       <p class="notice"><%= notice %></p>
+       <p class="alert"><%= alert %></p>
+
+  4. If you are deploying on Heroku with Rails 3.2 only, you may want to set:
+
+       config.assets.initialize_on_precompile = false
+
+     On config/application.rb forcing your application to not access the DB
+     or load models when precompiling your assets.
+
+  5. You can copy Devise views (for customization) to your app by running:
+
+       rails g devise:views
+=end
