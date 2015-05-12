637e1ceaa091cd36996874800f7a0a19374d6176
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-12 14:51:03 -0400

    changed some gems and routes

--- Gemfile
+++ Gemfile
@@ -30,6 +30,12 @@ group :development do
   gem 'sqlite3'
 end
 
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+    #this will nest the api under api.examplewebsite.com/
+    #worry about versioning later
+    scope module: :vi do
+      #developer shouldn't have to worry about version in URL.
+      #they should automatically use latest version
+      # to do this we have to change version access thru HTTP Headers
+
+      #also check out rocketpants and versionist gem to handle versioning configuration
+    end
 
   end
 
