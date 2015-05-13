2eafdd6897017d68af16dc5ba2c7fc39a844a98c
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-12 21:56:43 -0400

    added create controller action for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
     #this will nest the api under api.examplewebsite.com/
     #worry about versioning later
-    scope module: :vi do
+    scope module: :v1 do
       #developer shouldn't have to worry about version in URL.
       #they should automatically use latest version
       # to do this we have to change version access thru HTTP Headers
 
       #also check out rocketpants and versionist gem to handle versioning configuration
+      resources :users
     end
 
   end
