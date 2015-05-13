171113c335cc50135641597fea2bb43da4ba79c7
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-12 21:59:56 -0400

    added update action to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,4 +18,14 @@ class Api::V1::UsersController < ApplicationController
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status:200, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
 end
